9f32a7c9b8b137ac7b954c70dce3949f1ee51cee
fatemahalzahraa <fatemah.a.baqer@gmail.com>
2024-05-15 19:32:19 +0300

    solved tasks

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 function PetItem({ pet }) {
+  const [image, setImage] = useState(pet.image);
+  // note that image ^^ should be in the img that we want it to be changed, look down search to where 1st image is put
+  // it was in <img className="image" alt={pet.name} src={pet.image} /> , we changed the src to {image} that is in the useState ^^^^
+  const petThePet = () => {
+    setImage(pet.image2);
+  };
+
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
       <div className="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={image} />
         <div className="content">
           <h3>{pet.name}</h3>
-          <button type="button" className="btn btn-info">
+          <button onClick={petThePet} type="button" className="btn btn-info">
             Pet
           </button>
           <button type="button" class="btn btn-info  m-2">
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,8 +1,65 @@
+import { useState } from "react";
 import pets from "../petsData";
 import PetItem from "./PetItem";
 
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+  //we used a useState because we want the changes that we apply to be tracked always!
+  // searchQuery is a variable and setSearchQuery is a FUNCTION that changes the value of the variable.
+  const [searchQuery, setSearchQuery] = useState("");
+
+  //
+
+  const [type, setType] = useState("");
+
+  //NOTE!!
+  //because we want to filter the names we will choose pets.name in the if statement
+  //if we want to filter anything else we will use pet.thething instead!!
+  //NOTE THAT we want to give filteredPets a value >> its value is declared through something below (FIND IT OUT!!)
+  const filteredPets = pets.filter((pet) => {
+    if (pet.name.toLowerCase().includes(searchQuery.toLowerCase())) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  //
+  //we put filteredPets.filter instead of pets.filter because if the user didn't give us anything
+  //it will already iterate through PETS ^^ which is already in function above
+  //WE did that because we want if we choose the type Dog and the name of that dog, THAT dog is filtered!
+  //we basically chained both of them together! (this is done if we were asked for both names and types of pets to be filtered together to give a unique dog/cat/rabbit)
+  const filteredTypes = filteredPets.filter((pet) => {
+    if (pet.type.includes(type)) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  //!!!the function below was already included in the code YOU didn't write it!!!
+  //HOWEVER we don't want the whole pets to be mapped, we just want to map the FILTERED PETS which
+  //basically includes the NAME of the pet that we will write in the search bar!
+  // THAT IS WHY we changed the mapping from (pets.map) to (filteredPets.map)
+  //CHANGED!!!!!!!!!!!!!!>> we changed filteredPets.map to filteredTypes.map because we combined both together
+  const petList = filteredTypes.map((pet) => (
+    <PetItem pet={pet} key={pet.id} />
+  ));
+
+  //we used this function because we want to give the setSearchQuery a value "which is what we write in the search bar"
+  //but why we used setSearchQuery and not searchQuery? ASK!
+  //
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  //
+
+  const PetSelector = (event) => {
+    setType(event.target.value);
+  };
+
+  //NOTE : we used onChange property on our target (search bar) which is included in <input/>
+  //and we declared it by a function "^^^written above this comment^^^"
 
   return (
     <section id="doctors" className="doctor-section pt-140">
@@ -20,11 +77,12 @@ function PetsList() {
                   placeholder="Search"
                   aria-label="Search"
                   aria-describedby="search-addon"
+                  onChange={handleChange}
                 />
               </div>
               <br />
               Type:
-              <select className="form-select">
+              <select className="form-select" onChange={PetSelector}>
                 <option value="" selected>
                   All
                 </option>
